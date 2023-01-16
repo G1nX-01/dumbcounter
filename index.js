9998d90e4a1c6f3596bb49f6489a03bf09b528c5
G1nX-01 <81038333+G1nX-01@users.noreply.github.com>
2023-01-16 16:37:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const keepAlive = require('./server');
+const resetthing = require('./reset/code.js');
+const Monitor = require('ping-monitor');
+
+keepAlive();
+const monitor = new Monitor({
+    website: 'https://dumbasscounter.genarunchisacoa.repl.co/',
+    title: 'dumbass counter',
+    interval: 2
+});
+
+monitor.on('up', (res) => console.log(`${res.website} is up.`));
+monitor.on('down', (res) => console.log(`${res.website} died - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} stopped.`) );
+monitor.on('error', (error) => console.log(error));
+
 const express = require('express');
 var dumbass = 0
 const app = express();
@@ -8,12 +24,17 @@ app.get('/', (req, res) => {
   res.send(altdumbass)
   console.log(dumbass)
 });
+app.get('/reset', (req, res) => {
+  reset()
+  altdumbass = dumbass.toString()
+  res.send("woohoo! reset! we're at " + altdumbass + " now")
+});
 
 app.listen(3000, () => {
   console.log('server started');
 });
 
 function reset() {
- dumbass = 0
+dumbass = 0
 console.log("reset!")
 }
